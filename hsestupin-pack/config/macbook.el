b36f00d53df8ae64cfaffd164efefa9fe7a849e8
Stupin Sergey <stupin.sergey@gmail.com>
2013-05-09 17:43:17 +0400

    add some macbook hooks

---
+++ hsestupin-pack/config/macbook.el
@@ -0,0 +1,7 @@
+;; set fullscreen mode on startup
+(set-frame-parameter nil 'fullscreen 'fullboth)
+
+;; Macbook hooks :o
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'none)
+(global-set-key (kbd "M-`") 'ns-next-frame)
